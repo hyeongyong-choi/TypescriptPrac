d313b6745b04027612e9f0c3fb96de4a6f4c8f2d
hyeongyong-choi <guddyd6761@naver.com>
2022-11-24 12:04:35 +0900

    TypeScript 컴파일러

---
+++ ConfiguringTypeScript/src/index.ts
@@ -0,0 +1,28 @@
+interface Chicken {
+    breed: string;
+    eggsPerWeek:number;
+    name:string;
+    age:number
+}
+
+const norma: Chicken = {
+    breed:"Silkie",
+    eggsPerWeek:4,
+    name:"Norma",
+    age: 2
+}
+
+const Juniper: Chicken = {
+    breed:"Silkie",
+    eggsPerWeek:4,
+    name:"Juniper",
+    age: 1
+}
+
+// function doThing(thing){
+//     console.log(thing)
+// }
+
+let users: string[] | null = ["asds","safew"]
+users = null;
+
